58bef81f8ff97081abccf83e2a39d284a1d03014
Michael Queyrichon <michael.queyrichon@digdir.no>
2024-01-29 14:14:16 +0100

    Fix integration tests

--- integrationtests/curl-test.sh
+++ integrationtests/curl-test.sh
@@ -3,11 +3,11 @@ kubectl wait deployment -n default kuberneteswrapper --for condition=Available=T
 kubectl port-forward svc/kuberneteswrapper 8080:8080 &
 PORTFORWARD_PID=$!
 sleep 5 # Wait for portforward to be ready
-expectedDeployment='[{"version":"1.23.2-alpine","release":"dummy-deployment"},{"version":"local","release":"kuberneteswrapper"}]'
-expectedDaemonsets='[{"version":"1.23.2-alpine","release":"dummy-daemonset"}]'
-actualDeployment=$(curl http://localhost:8080/api/v1/deployments --silent)
-test $expectedDeployment = $actualDeployment
-if [ $? -gt 0 ]
+expectedDeployment='[{"version":"1.23.2-alpine","release":"dummy-deployment","status":{"availableReplicas":1,"collisionCount":null,"conditions":[{"message":"Deployment has minimum availability.","reason":"MinimumReplicasAvailable","status":"True","type":"Available"},{"message":"ReplicaSet \"dummy-deployment-b9b48b788\" has successfully progressed.","reason":"NewReplicaSetAvailable","status":"True","type":"Progressing"}],"observedGeneration":1,"readyReplicas":1,"replicas":1,"unavailableReplicas":null,"updatedReplicas":1},"replicas":1},{"version":"local","release":"kuberneteswrapper","status":{"availableReplicas":1,"collisionCount":null,"conditions":[{"message":"Deployment has minimum availability.","reason":"MinimumReplicasAvailable","status":"True","type":"Available"},{"message":"ReplicaSet \"kuberneteswrapper-6d4c57fc98\" has successfully progressed.","reason":"NewReplicaSetAvailable","status":"True","type":"Progressing"}],"observedGeneration":1,"readyReplicas":1,"replicas":1,"unavailableReplicas":null,"updatedReplicas":1},"replicas":1}]'
+expectedDaemonsets='[{"version":"1.23.2-alpine","release":"dummy-daemonset","status":null,"replicas":null}]'
+actualDeployment=$(curl http://localhost:8080/api/v1/deployments --silent | awk '{gsub(/"lastTransitionTime":"[^"]+",/,""); gsub(/"lastUpdateTime":"[^"]+",/,"")}1')
+
+if [[ $expectedDeployment = $actualDeployment ]]
 then
   echo "Deployment test failed"
   echo "Exp: $expectedDeployment"
@@ -18,8 +18,7 @@ else
 fi
 
 actualDaemonsets=$(curl http://localhost:8080/api/v1/daemonsets --silent)
-test $expectedDaemonsets = $actualDaemonsets
-if [ $? -gt 0 ]
+if [[ $expectedDaemonsets = $actualDaemonsets ]]
 then
   echo "DaemonsetsTest failed"
   echo "Exp: $expectedDaemonsets"
