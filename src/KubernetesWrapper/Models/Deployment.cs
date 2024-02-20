283e43f62c4d79cb9e9143621f3f8444d752894f
Michael Queyrichon <michael.queyrichon@digdir.no>
2024-02-20 12:12:50 +0100

    Revert "Remove availabilityPercentage"
    This reverts commit effaf915b5f5634f16860fb4dc38af2e02713e73.

--- src/KubernetesWrapper/Models/Deployment.cs
+++ src/KubernetesWrapper/Models/Deployment.cs
@@ -12,5 +12,16 @@ namespace KubernetesWrapper.Models
         /// This represents the current state of the deployment in the Kubernetes cluster, such as 'Available', 'Progressing', 'Failed', etc.
         /// </summary>
         public string Status { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the resource is available.
+        /// </summary>
+        public bool Available { get; set; }
+
+        /// <summary>
+        /// Gets or sets the availability percentage of the deployment.
+        /// This represents the percentage of pods that are up and running compared to the total number of pods in the deployment.
+        /// </summary>
+        public int AvailabilityPercentage { get; set; }
     }
 }
